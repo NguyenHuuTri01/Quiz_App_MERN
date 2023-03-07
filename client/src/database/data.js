dd6c8c28ede72d1c60864742cd8b9431ca14cfce
Nguyen Huu Tri <nguyenhuutri0905@gmail.com>
2023-03-07 10:29:28 +0700

    edit data.js

--- client/src/database/data.js
+++ client/src/database/data.js
@@ -1,31 +1,54 @@
 export default [
     {
         id: 1,
-        question: "Javascript is an ___________language",
+        question: "Trong Javascript sự kiện OnUnload thực hiện khi nào?",
         options: [
-            'object-oriented',
-            'object-based',
-            'procedural',
+            'Khi bắt đầu chương trình chạy',
+            'Khi click chuột',
+            'Khi kết thúc một chương trình',
+            'Khi di chuyển chuột qua',
         ]
     },
     {
         id: 2,
-        question: "Following methods can be used to display data in some form using javascript",
+        question: "Trong Javascript sự kiện Onblur thực hiện khi nào?",
         options: [
-            'document.write()',
-            'console.log()',
-            'window.alert()',
+            'Khi một đối tượng trong form mất focus',
+            'Khi một đối tượng trong form có focus',
+            'Khi di chuyển con chuột qua form',
+            'Khi click chuột vào nút lệnh'
         ]
     },
     {
         id: 3,
-        question: "When an operator value is NULL, the typeof returned by the unary operator is:",
+        question: "Trong Javascript sự kiện OnMouseOver thực hiện khi nào?",
         options: [
-            'Boolean',
-            'Undefined',
-            'Object',
+            'Khi một đối tượng trong form mất focus',
+            'Khi một đối tượng trong form có focus',
+            'Khi di chuyển con chuột qua một đối tượng trong form',
+            'Khi click chuột vào nút lệnh'
+        ]
+    },
+    {
+        id: 4,
+        question: "Trong Javascript sự kiện Onclick xảy ra khi nào?",
+        options: [
+            'Khi một đối tượng trong form mất focus',
+            'Khi một đối tượng trong form có focus',
+            'Khi click chuột vào một đối tượng trong form',
+            'Khi click chuột vào nút lệnh'
+        ]
+    },
+    {
+        id: 5,
+        question: "Trong Javascript sự kiện Onchange thực hiện khi nào?",
+        options: [
+            'Khi một đối tượng trong form mất focus',
+            'Khi một đối tượng trong form có focus',
+            'Xảy ra khi giá trị của một trường trong form được người dùng thay đổi',
+            'Khi click chuột vào nút lệnh'
         ]
     },
 ]
 
-export const answers = [0, 1, 2];
+export const answers = [2, 0, 2, 3, 2];
